f7513cb58f241404c1ada981cdf52e94a3436fde
electros15 <electros15921@gmail.com>
2017-06-26 17:07:11 +0200

    Add files via upload

--- equation solver/src/com/solver/Analyse/Interpretation.java
+++ equation solver/src/com/solver/Analyse/Interpretation.java
@@ -1,34 +1,45 @@
 package com.solver.Analyse;
 
+import java.util.ArrayList;
+
 import com.solver.data.Equa;
 
 public class Interpretation {
-
-	Equa e = new Equa();
+	
+		public static Equa interpretation(String s){
+			char[] tabIn = s.toCharArray();
+			
+			char tabOut1[] = multiHide(tabIn);
+			Equa e = new Equa();
+			
+			
 		
-	    public static Equa multiHide(String s){
-
-	    	char[] tab = s.toCharArray();
-	    	char[] tabOut = s.toCharArray();
+			
+			
+			
+			return e;
+		}
+		
+	    public static char[] multiHide(char tabIn[]){
 	    	int y = 0;
+	    	char tabOut2[] = new char[tabIn.length*2];
 	    	
-	    	for(int i = 0; i <= tab.length ; i = i+1 ) {
-				if(tab[i] == '(' || tab [i] == '[') {
-					String d = tab[i]+"";
+	    	for(int i = 0; i <= tabIn.length ; i = i+1 ) {
+				if(tabIn[i] == '(' || tabIn[i] == '[') {
+					String d = tabIn[i]+"";
 					if(verifyNL.verifyNb(d) == true || verifyNL.verifyNb(d) == true) {
-						for(int u = 0; u <= tab.length + 1 ; u = u+1) {
+						for(int u = 0; u <= tabIn.length + 1 ; u = u+1) {
 							if(u == i) {
-								tabOut[u] = '*';
+								tabOut2[u] ='*';
 								u = u+1 ;
 							}
-							tabOut[u] = tab[y];
+							tabOut2[u] = '*';
 							y = y+1 ;
 						}
 					}
 				}
 	    	}
+	    	return tabOut2;
 	    }
-	    
-	Equa();
 	}
 
